4a40ff9f0519eeb0ebd3e16600b1853ecc76b40d
kgsensei <jt@kellem.com>
2021-12-18 21:52:41 -0500

    test 1.2

--- homepage.js
+++ homepage.js
@@ -3,10 +3,12 @@ checkedvar=localStorage.getItem('enabled')
 if(checkedvar==null||checkedvar==undefined){
 	localStorage.setItem('enabled','yes')
 	document.getElementById("enabled_checkbox").checked=1
-}else{
+}
+
+$(document).ready(function(){
 	if(checkedvar=='yes'){document.getElementById("enabled_checkbox").checked=1}
 	if(checkedvar=='no'){document.getElementById("enabled_checkbox").checked=0}
-}
+})
 
 $("#enabled_checkbox").click(function(){
 	element=document.getElementById("enabled_checkbox")
