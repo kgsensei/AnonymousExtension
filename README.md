d204cafdb1dd85ee497cbaf61ebea4edac8b3e90
kgsensei <jt@kellem.com>
2022-08-09 23:13:21 -0400

    Start working on Manifest v3

--- README.md
+++ README.md
@@ -9,17 +9,24 @@ AnonymousExtension is a Edge and Chrome browser extension designed to make the i
 Get it today on the Edge Store: [HERE](https://microsoftedge.microsoft.com/addons/detail/anonymous-extension/cdiiogmchafjebbdjgbchpdeoghfbocp)
 If you need help setting up join the support discord: [here](https://discord.gg/U5A3QWXZKZ).
 
+## DEV UPDATE
+
+Since the announcement of Manifest v3 I have update the extension multiple times to try and 
+accomidate for this change. In recent updates the filtering system has become much more strict
+and may end up needing a recode. Please let me know if any issues arrise. The enable/disable
+feature will not work for a little while.
+
 Here are just some of the things it blocks:
 
-#### Top 5 Trackers
+## Top 5 Trackers
 
-- Google Analytics
-- Raygun.io
-- Ably.io
-- DoubleClick.net
-- Sentry.io
+- analytics.google.com
+- raygun.io
+- ably.io
+- doubleclick.net
+- sentry.io
 
-#### Top 5 IP Grabbers
+## Top 5 IP Grabbers
 
 - grabify.link
 - shrekis.life
@@ -27,22 +34,22 @@ Here are just some of the things it blocks:
 - iplogger.org
 - linkify.me
 
-#### Top 5 Site-Specific Trackers
+## Top 5 Site-Specific Trackers
 
-- Tiktok.com
-- Discord.com
-- Analytics.Twitter.com
-- Spotify.com
-- Stats.Pandora.com
+- tiktok.com
+- discord.com
+- analytics.twitter.com
+- spotify.com
+- stats.pandora.com
 
-#### Top 5 Ad Trackers/Providers
+## Top 5 Ad Trackers/Providers
 
-- Amazon-Adsystem.com
-- Advertising.com
-- Adform.net
-- Adservice.Google.com
-- Googlesyndication.com ?
+- amazon-adsystem.com
+- advertising.com
+- adform.net
+- adservice.google.com
+- doubleclick.net
 
-## Legal:
+## Legal
 
 Copyright &copy; kgsensei 2022.
--- homepage.js
+++ homepage.js
@@ -1,68 +1,41 @@
-$(document).ready(function(){
-	fadeAnimationTime=1000
-	checkedvar=localStorage.getItem('enabled')
+$(document).ready(() => {
+	animTime = 275
+	checkedvar = localStorage.getItem('enabled')
 
-	if(checkedvar==null||checkedvar==undefined){
-		localStorage.setItem('enabled','yes')
-		document.getElementById("enabled_checkbox").checked=1
+	if(checkedvar == null || checkedvar == undefined){
+		localStorage.setItem('enabled', 'yes')
+		document.getElementById("enabled_checkbox").checked = 1
 	}
 
-	if(checkedvar=='yes'){document.getElementById("enabled_checkbox").checked=1}
-	if(checkedvar=='no'){document.getElementById("enabled_checkbox").checked=0}
+	if(checkedvar == 'yes') {document.getElementById("enabled_checkbox").checked = 1}
+	if(checkedvar == 'no') {document.getElementById("enabled_checkbox").checked = 0}
 
-	$("#enabled_checkbox").click(function(){
+	$("#enabled_checkbox").click(() => {
 		element=document.getElementById("enabled_checkbox")
-		if(element.checked){
-			localStorage.setItem('enabled','yes')
-			$('#fs_0').fadeIn(fadeAnimationTime)
-			document.getElementById('fs_0').innerHTML='Enabled'
-			$('#fs_0').fadeOut(fadeAnimationTime)
-		}else{
+		if(element.checked) {
+			localStorage.setItem('enabled', 'yes')
+			$('#fs_0').fadeIn(animTime)
+			document.getElementById('fs_0').innerHTML = 'Enabled'
+			$('#fs_0').fadeOut(animTime)
+		} else {
 			localStorage.setItem('enabled','no')
-			$('#fs_0').fadeIn(fadeAnimationTime)
-			document.getElementById('fs_0').innerHTML='Disabled'
-			$('#fs_0').fadeOut(fadeAnimationTime)
+			$('#fs_0').fadeIn(animTime)
+			document.getElementById('fs_0').innerHTML = 'Disabled'
+			$('#fs_0').fadeOut(animTime)
 		}
-	})
-
-	$("#clear_data_now_btn").click(function(){
-		var archive=[],keys=Object.keys(localStorage),i=0,key
-		for(;key=keys[i];i++){if(key.substr(0,3)=='tb-'){
-			archive.push(key)
-			localStorage.removeItem(key)
-		}}
-		$('#fs_1').fadeIn(fadeAnimationTime)
-		document.getElementById('fs_1').innerHTML='Removed '+archive.length+' Items'
-		$('#fs_1').fadeOut(fadeAnimationTime)
 	})
 })
 
-String.prototype.toLocation=function(){
-    var a=document.createElement('a')
-	a.href=this
-	return a.hostname
-}
-
-function checkTrackersBlocked(){
-	trackers=localStorage.getItem('tb-'+localStorage.getItem('lastTabID'))
-	targturl=localStorage.getItem('lastTabUR').toLocation()
-	document.getElementById('url_place').innerHTML=targturl
-	if(trackers!=''){document.getElementById('trackers_place').innerHTML=trackers}
-	else{document.getElementById('trackers_place').innerHTML='?'}
-	setTimeout(function(){checkTrackersBlocked()},500)
-}
-
-function localizeHtmlPage(){
-    var objects=document.getElementsByTagName('html')
-    for(var j=0;j<objects.length;j++){
-        var obj=objects[j]
-        var valStrH=obj.innerHTML.toString()
-        var valNewH=valStrH.replace(/__MSG_(\w+)__/g,function(match,v1){
-            return v1?chrome.i18n.getMessage(v1):""
+function localizeHtmlPage() {
+    var objects = document.getElementsByTagName('html')
+    for(var j = 0; j < objects.length; j++) {
+        var obj = objects[j]
+        var valStrH = obj.innerHTML.toString()
+        var valNewH = valStrH.replace(/__MSG_(\w+)__/g, (match, v1) => {
+            return v1 ? chrome.i18n.getMessage(v1) : ""
         })
-        if(valNewH!=valStrH){obj.innerHTML=valNewH}
+        if(valNewH != valStrH) {obj.innerHTML = valNewH}
     }
 }
 
 localizeHtmlPage()
-checkTrackersBlocked()
