a556502829834a4ec2be46b78f66e598e206fbab
kgsensei <jt@kellem.com>
2022-10-14 21:33:34 -0400

    v3.1

--- background.js
+++ background.js
@@ -1,82 +1,47 @@
-const LS = {
-	getAllItems: () => chrome.storage.local.get(),
-	getItem: async key => (await chrome.storage.local.get(key))[key],
-	setItem: (key, val) => chrome.storage.local.set({[key]: val}),
-	removeItems: keys => chrome.storage.local.remove(keys),
-}
-
-const h = "https://raw.githubusercontent.com/kgsensei/AnonymousExtension/master/hosts/"
-
-async function updateDomainLists() {
-	var x = ["a_list.txt|a_list", "i_list.txt|i_list", "t_list.txt|t_list"]
-	for(let i = 0; i < x.length; i++) {
-		let y = x[i].split('|')
-		await fetch(h + y[0])
-		.then((r) => r.text())
-		.then(async (r) => {
-			LS.setItem(y[1], r)
-		})
-	}
-	buildRuleConditions()
-}
-
-fetch(h + "vrCh.txt")
-.then((r) => r.text())
-.then(async(r) => {
-	if(r != await LS.getItem("v")) {
-		updateDomainLists()
-		LS.setItem("v", r)
-	} else {
-		buildRuleConditions()
-	}
-})
-
-async function buildRuleConditions() {
-	var x = ["a_list", "i_list", "t_list"]
-	var r = []
-	for(let i = 0; i < x.length; i++) {
-		var z = await LS.getItem(x[i])
-		var u = z.split('\n')
-		u.forEach((d, j) => {
-			if(d != '') {
-				r.push({
-					"id": `${i+1}${j}`*1,
-					"priority": 1,
-					"action": {
-						"type": "block"
-					},
-					"condition": {
-						"urlFilter": d,
-						"resourceTypes": [
-							"font",
-							"ping",
-							"other",
-							"image",
-							"media",
-							"object",
-							"script",
-							"sub_frame",
-							"webbundle",
-							"websocket",
-							"csp_report",
-							"main_frame",
-							"stylesheet",
-							"webtransport",
-							"xmlhttprequest"
-						]
-					}
-				})
-			}
-		})
-		
-	}
-	chrome.declarativeNetRequest.getDynamicRules(e => {
-		const p = e.map(rule => rule.id)
-		chrome.declarativeNetRequest.updateDynamicRules(
-			{
-				removeRuleIds: p,
-				addRules: r
-			}
-		)
-	})
-}
+f={
+	getAllItems:()=>chrome.storage.local.get(),
+	getItem:async(key)=>(await chrome.storage.local.get(key))[key],
+	setItem:(key,val)=>chrome.storage.local.set({[key]:val}),
+	removeItems:(keys)=>chrome.storage.local.remove(keys)}
+h="https://raw.githubusercontent.com/kgsensei/AnonymousExtension/master/hosts/"
+r=["a_list","i_list","t_list",".txt|"]
+udl = async() => {
+	var x=[r[0]+r[3]+r[0],r[1]+r[3]+r[1],r[2]+r[3]+r[2]]
+	for(let i=0;i<x.length;i++){
+		let y=x[i].split('|')
+		await fetch(h+y[0])
+		.then((r)=>r.text())
+		.then(async(r)=>{f.setItem(y[1],r)})
+	}brc()}
+brc = async() => {
+	var x=[r[0],r[1],r[2]]
+	var j=[]
+	for(let i=0;i<x.length;i++){
+		var z=await f.getItem(x[i])
+		var u=z.split('\n')
+		u.forEach((d,k)=>{
+			if(d!=''){
+				j.push({
+					"id":`${i+1}${k}`*1,
+					"priority":1,
+					"action":{"type":"block"},
+					"condition":{
+						"urlFilter":d,
+						"resourceTypes":[
+							"font","ping","other",
+							"webbundle","websocket",
+							"image","media","object",
+							"csp_report","main_frame",
+							"stylesheet","webtransport",
+							"script","sub_frame","xmlhttprequest",							
+						]}})}})}
+	chrome.declarativeNetRequest.getDynamicRules(e=>{
+		const p=e.map(rule=>rule.id)
+		chrome.declarativeNetRequest.updateDynamicRules({removeRuleIds:p,addRules:j})})}
+fetch(h+"vrCh.txt")
+.then((r)=>r.text())
+.then(async(r)=>{
+	if(r!=await f.getItem("v")){
+		udl()
+		f.setItem("v", r)
+	}else{brc()}})
