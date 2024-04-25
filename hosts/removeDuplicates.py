2aaf22ccd590a8ad752bb3aa5a2f47c6509ad7eb
kgsensei <jt@kellem.com>
2024-04-24 21:31:37 -0400

    Improve adblock functionality

--- hosts/removeDuplicates.py
+++ hosts/removeDuplicates.py
@@ -1,10 +1,10 @@
 terms = []
-with open('a_list.txt') as f:
+
+with open('blacklist.txt') as f:
     terms += f.read().split('\n')
-with open('i_list.txt') as f:
-    terms += f.read().split('\n')
-with open('t_list.txt') as f:
-    terms += f.read().split('\n')
+
 dup = [i for i, x in enumerate(terms) if terms.count(x) > 1]
+
 for i in dup:
-    print("DOUBLE:", terms[i])
+    if terms[i] != "" and terms[i][0] != "~":
+        print("DOUBLE:", terms[i])
